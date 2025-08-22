12d8c445da1e8c21fd9494b0726c31d3d090e604
Aarizia <u2469964@rts-365.dk>
2025-08-22 14:06:40 +0200

    feat: create FullScreenLayout component for fullscreen rendering chore: remove unused import statement from CommonLayout

--- src/app/(commonlayout)/layout.jsx
+++ src/app/(commonlayout)/layout.jsx
@@ -1,4 +1,3 @@
-/* import { Geist, Geist_Mono } from "next/font/google"; */
 import SiteHeader from "@/components/site-header";
 import SiteFooter from "@/components/site-footer";
 
---
+++ src/app/(fullscreenlayout)/layout.jsx
@@ -0,0 +1,5 @@
+export default function FullScreenLayout({ children }) {
+  return (
+      {children}
+  );
+}
